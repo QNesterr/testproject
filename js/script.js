47e6a047d39d8c89e27d7125c029e265df6e7a10
light-flight <yurievg@mail.ru>
2015-05-22 00:33:22 +0300

    svg graphics and scroolly.js were added

--- js/script.js
+++ js/script.js
@@ -16,19 +16,78 @@ $(document).ready(function() {
             left: '-160px'
         }, 800, 'easeOutBounce');
     });
-     // ------------------------------------
+    // ----------------------------------------
     // ДЛЯ МЕНЮ -------------------------------
+    var AnimationIsGoing = false;
     $('.menu-button').click(function() {
-        $('.menu').animate({
-            left: '0px'
-        }, 800, 'easeInOutCubic');
-        $('.menu-button').fadeToggle(500, 'swing');
+        if (AnimationIsGoing == false) {
+            AnimationIsGoing = true;
+
+            $('#side-bar').animate({
+                left: '0px'
+            }, 800, 'easeInOutCubic');
+            
+            $('#menu-panel').fadeToggle(400, 'swing');
+            setTimeout(function() {AnimationIsGoing = false;}, 800);
+        }
     });
-    $('.menu').on('swipeleft', function() {
-        $('.menu').animate({
-            left: '-230px'
-        }, 800, 'easeInOutCubic');
-        $('.menu-button').fadeToggle(500, 'swing');
+    $('#side-bar').on('swipeleft', function() {
+        if (AnimationIsGoing == false) {
+            AnimationIsGoing = true;
+
+            $('#side-bar').animate({
+                left: '-230px'
+            }, 800, 'easeInOutCubic');
+            
+            $('#menu-panel').fadeToggle(1000, 'swing');
+            setTimeout(function() {AnimationIsGoing = false;}, 800);
+        }
     });
+    // ----------------------------------------
+    // ДЛЯ СКРОЛЛ ЭФФЕКТОВ
+    $('#menu-panel').scroolly([{
+        from: 'doc-top',
+        to: 'doc-top + 100el',
+        cssFrom: {
+            'height': '80px'
+        },
+        cssTo: {
+            'height': '60px'
+        }
+    }]);
+    $('.header-image').scroolly([{
+        from: 'doc-top',
+        to: 'doc-top + 100el',
+        cssFrom: {
+            'height': '80px',
+            'margin-left': '100px'
+        },
+        cssTo: {
+            'height': '60px',
+            'margin-left': '55px'
+        }
+    }]);
+    $('.menu-button').scroolly([{
+        from: 'doc-top',
+        to: 'doc-top + 100el',
+        cssFrom: {
+            'height': '60px',
+            'width': '60px'
+        },
+        cssTo: {
+            'height': '30px',
+            'width': '30px'
+        }
+    }]);
+    $('.menu-button hr').scroolly([{
+        from: 'doc-top',
+        to: 'doc-top + 100el',
+        cssFrom: {
+            'margin-top': '12px'
+        },
+        cssTo: {
+            'margin-top': '6px'
+        }
+    }]);
     // ----------------------------------------
 });
