7bfecd959879855e0da581e435861f0c1cc2ebfc
light-flight <yurievg@mail.ru>
2015-05-22 01:05:56 +0300

    responsive grid, images

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
 $(document).ready(function() {
-    // ДЛЯ ЗАКЛАДОЧЕК -------------------------
-    $('.tab').click(function() {
-        if ($(this).css('left') == '-160px') {
-            $(this).animate({
-                left: '-10px'
-            }, 800, 'easeInOutCubic');
-        } else {
-            $(this).animate({
-                left: '-160px'
-            }, 800, 'easeOutBounce');
-        }
-    });
-    $('.tab').on('swipeleft', function() {
-        $(this).animate({
-            left: '-160px'
-        }, 800, 'easeOutBounce');
-    });
-    // ----------------------------------------
-    // ДЛЯ МЕНЮ -------------------------------
+//-----------------------------------------------------------------------------
+//        ОСНОВНОЕ МЕНЮ
     var AnimationIsGoing = false;
     $('.menu-button').click(function() {
         if (AnimationIsGoing == false) {
@@ -36,58 +19,53 @@ $(document).ready(function() {
             AnimationIsGoing = true;
 
             $('#side-bar').animate({
-                left: '-230px'
+                left: '-260px'
             }, 800, 'easeInOutCubic');
             
             $('#menu-panel').fadeToggle(1000, 'swing');
             setTimeout(function() {AnimationIsGoing = false;}, 800);
         }
     });
-    // ----------------------------------------
-    // ДЛЯ СКРОЛЛ ЭФФЕКТОВ
-    $('#menu-panel').scroolly([{
-        from: 'doc-top',
-        to: 'doc-top + 100el',
-        cssFrom: {
-            'height': '80px'
-        },
-        cssTo: {
-            'height': '60px'
+
+//-----------------------------------------------------------------------------
+//        ЗАКЛАДОЧКИ
+    $('.tab').click(function() {
+        if ($(this).css('left') == '-160px') {
+            $(this).animate({
+                left: '-10px'
+            }, 800, 'easeInOutCubic');
+        } else {
+            $(this).animate({
+                left: '-160px'
+            }, 800, 'easeOutBounce');
         }
-    }]);
-    $('.header-image').scroolly([{
-        from: 'doc-top',
-        to: 'doc-top + 100el',
-        cssFrom: {
-            'height': '80px',
-            'margin-left': '100px'
-        },
-        cssTo: {
-            'height': '60px',
-            'margin-left': '55px'
-        }
-    }]);
-    $('.menu-button').scroolly([{
-        from: 'doc-top',
-        to: 'doc-top + 100el',
-        cssFrom: {
-            'height': '60px',
-            'width': '60px'
-        },
-        cssTo: {
-            'height': '30px',
-            'width': '30px'
-        }
-    }]);
-    $('.menu-button hr').scroolly([{
-        from: 'doc-top',
-        to: 'doc-top + 100el',
-        cssFrom: {
-            'margin-top': '12px'
-        },
-        cssTo: {
-            'margin-top': '6px'
-        }
-    }]);
-    // ----------------------------------------
+    });
+    $('.tab').on('swipeleft', function() {
+        $(this).animate({
+            left: '-160px'
+        }, 800, 'easeOutBounce');
+    });
+
+//-----------------------------------------------------------------------------
+//        СКРОЛЛ ЭФФЕКТ ДЛЯ ВЕРХНЕЙ ПАНЕЛИ
+    $('#menu-panel').scroolly([
+                {
+                    from: 'doc-top + 100el',
+                    to: 'doc-bottom',
+                    direction: 1,
+                    css: {
+                        //'-transition': 'all 0.5s',
+                        top: '-50px'
+                    }
+                },
+                {
+                    from: 'doc-top',
+                    to: 'doc-bottom',
+                    direction: -1,
+                    css: {
+                        //'-transition': 'all 0.5s',
+                        top: '0px'
+                    }
+                }
+            ]);
 });
